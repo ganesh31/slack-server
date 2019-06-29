ba7ffd0c992ebab2f18e257450be2e771f51302e
G1, Ganesh <Ganesh.G1@sc.com>
2019-06-29 19:19:02 +0530

    feat(auth): Add permissions for accessing restricted api

--- src/graphql/resolvers/team.js
+++ src/graphql/resolvers/team.js
@@ -1,20 +1,22 @@
 import { formatErrors } from '../../utils';
-
+import { requiresAuth } from '../../permissions';
 export default {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      try {
-        await models.Team.create({ ...args, owner: user.id });
-        return {
-          ok: true,
-        };
-      } catch (error) {
-        console.log(error);
-        return {
-          ok: false,
-          errors: formatErrors(error, models),
-        };
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            ok: true
+          };
+        } catch (error) {
+          console.log(error);
+          return {
+            ok: false,
+            errors: formatErrors(error, models)
+          };
+        }
       }
-    },
-  },
+    )
+  }
 };
